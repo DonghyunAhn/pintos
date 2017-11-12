90cfb52edace46838f0d43786667f5a60c016912
cs330 <segaukwa@kaist.ac.kr>
2017-11-12 17:01:13 +0900

    src/src do not needed, Nov 11

--- src/src/vm/build/userprog/pagedir.d
+++
@@ -1,7 +0,0 @@
-userprog/pagedir.o: ../../userprog/pagedir.c ../../userprog/pagedir.h \
-  ../../lib/stdbool.h ../../lib/stdint.h ../../lib/stddef.h \
-  ../../lib/string.h ../../threads/init.h ../../lib/debug.h \
-  ../../threads/pte.h ../../threads/vaddr.h ../../threads/loader.h \
-  ../../threads/palloc.h ../../vm/frame.h ../../threads/thread.h \
-  ../../lib/kernel/hash.h ../../lib/kernel/list.h ../../lib/kernel/list.h \
-  ../../threads/synch.h ../../threads/palloc.h
--- src/src/vm/build/vm/page.d
+++
@@ -1,9 +0,0 @@
-vm/page.o: ../../vm/page.c ../../lib/kernel/hash.h ../../lib/stdbool.h \
-  ../../lib/stddef.h ../../lib/stdint.h ../../lib/kernel/list.h \
-  ../../lib/string.h ../../lib/kernel/hash.h ../../threads/synch.h \
-  ../../lib/kernel/list.h ../../threads/malloc.h ../../lib/debug.h \
-  ../../threads/palloc.h ../../threads/vaddr.h ../../threads/loader.h \
-  ../../userprog/pagedir.h ../../vm/page.h ../../vm/frame.h \
-  ../../threads/thread.h ../../threads/synch.h ../../vm/swap.h \
-  ../../devices/disk.h ../../lib/inttypes.h ../../filesys/file.h \
-  ../../filesys/off_t.h
